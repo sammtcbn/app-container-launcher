a552710a8821e4265734bb153321f206bd0c8e92
sammtcbn <sammtcbn@gmail.com>
2025-03-08 13:48:36 +0800

    script to launch ubuntu 22.04 and 24.04

---
+++ ubuntu2204.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run -it --rm --name ubt2204 ubuntu:22.04 bash
+#docker run -it --rm --name ubt2204 -v $(pwd):/root --workdir /root ubuntu:22.04 bash
---
+++ ubuntu2404.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run -it --rm --name ubt2404 ubuntu:24.04 bash
+#docker run -it --rm --name ubt2404 -v $(pwd):/root --workdir /root ubuntu:24.04 bash
