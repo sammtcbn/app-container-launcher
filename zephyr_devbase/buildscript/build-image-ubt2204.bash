99fc189418877dba15582c3c02a690cbb599d967
sammtcbn <sammtcbn@gmail.com>
2025-03-24 18:47:52 +0800

    minor update

--- zephyr_devbase/buildscript/build-image-ubt2204.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t sammtcbn/zephyr_devbase -f Dockerfile-ubt2204 . --no-cache
-
-echo
-
-echo docker image ls -a
-docker image ls -a
---
+++ zephyr_devbase/buildscript/build-image.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker build -t sammtcbn/zephyr_devbase -f Dockerfile . --no-cache
+
+echo
+
+echo docker image ls -a
+docker image ls -a
