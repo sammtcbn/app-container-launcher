8d28c085a4bb38a252fe6a1a0775dbca974a9104
sammtcbn <sammtcbn@gmail.com>
2025-04-23 01:57:54 +0800

    use 8080 as host port for nginx container

--- nginx_deattach.bash
+++ nginx_deattach.bash
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker run -d --name nginx1 -p 80:80 nginx
+
+#host_port=80
+host_port=8080
+
+container_port=80
+
+docker run -d --name mynginx1 -p ${host_port}:${container_port} nginx
+
+echo "run below command to visit nginx server"
+echo "    curl http://127.0.0.1:${host_port}/"
